f9a900d48ee50045031e76c0c7d33cc1ebba3730
Maxime Dupont <m.dupont103@gmail.com>
2024-05-17 10:14:19 +0200

    fix test coverage html & string methods

--- test/index/html.test.ts
+++ test/index/html.test.ts
@@ -2,31 +2,47 @@ import { Rule, parse } from '../../src';
 import { html } from './data';
 
 describe('html', () => {
-    it('html-query-selector & html-text', async () => {
-        const config = {
-            method: 'sequence',
-            sequence: [
-                { method: 'parse-to-html' },
-                { method: 'html-query-selector', selector: 'html body p' },
-                { method: 'html-text' }
-            ]
-        } satisfies Rule;
-        const value = html;
-        const result = 'Hello world!';
-        expect(await parse(config, value)).toStrictEqual(result);
-    });
+	it('html-query-selector & html-text - defined', async () => {
+		const config = {
+			method: 'sequence',
+			sequence: [
+				{ method: 'parse-to-html' },
+				{ method: 'html-query-selector', selector: 'html body p' },
+				{ method: 'html-text' }
+			]
+		} satisfies Rule;
+		const value = html;
+		const result = 'Hello world!';
+		expect(await parse(config, value)).toStrictEqual(result);
+	});
 
-    it('html-query-selector-all & html-attribute', async () => {
-        const config = {
-            method: 'sequence',
-            sequence: [
-                { method: 'parse-to-html' },
-                { method: 'html-query-selector-all', selector: 'html body ul li' },
-                { method: 'iterate', iterate: { method: 'html-attribute', attribute: 'title' } }
-            ]
-        } satisfies Rule;
-        const value = html;
-        const result = ['raspberry', 'blackcurrant', 'gooseberry'];
-        expect(await parse(config, value)).toStrictEqual(result);
-    });
+	it('html-query-selector & html-text - undefined', async () => {
+		const config = {
+			method: 'sequence',
+			sequence: [
+				{ method: 'parse-to-html' },
+				{ method: 'html-query-selector', selector: 'html body p ul' },
+				{ method: 'html-query-selector', selector: 'li' },
+				{ method: 'html-query-selector-all', selector: 'li' },
+				{ method: 'html-text' }
+			]
+		} satisfies Rule;
+		const value = html;
+		const result = undefined;
+		expect(await parse(config, value)).toStrictEqual(result);
+	});
+
+	it('html-query-selector-all & html-attribute', async () => {
+		const config = {
+			method: 'sequence',
+			sequence: [
+				{ method: 'parse-to-html' },
+				{ method: 'html-query-selector-all', selector: 'html body ul li' },
+				{ method: 'iterate', iterate: { method: 'html-attribute', attribute: 'title' } }
+			]
+		} satisfies Rule;
+		const value = html;
+		const result = ['raspberry', 'blackcurrant', 'gooseberry'];
+		expect(await parse(config, value)).toStrictEqual(result);
+	});
 });
